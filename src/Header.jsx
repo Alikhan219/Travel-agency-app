cee98268907d00e30fe6f6b5855d24760436b217
Alikhan219 <merabhai786786@gmail.com>
2020-12-20 18:33:13 -0800

    importing the google font and styling the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,18 @@ import React from "react"
 const Header= ()=>{
    
 return(
+  <>
+    <nav>
+        <label className="logo">TRVL</label>
+        <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </nav>
     
+  </>
 )
 
 }
