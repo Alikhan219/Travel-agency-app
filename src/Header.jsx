5151c06181637268b5b23063b11eaf1c98ec6124
Alikhan219 <merabhai786786@gmail.com>
2020-12-22 04:09:17 -0800

    working on beautiful header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,34 @@
-import React from "react"
+import React from "react";
 
-
-const Header= ()=>{
-   
-return(
-  <>
-    <nav>
+const Header = () => {
+  return (
+    <>
+      <nav>
+      <input type="checkbox" id="check" for="check" ></input>
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        
         <label className="logo">TRVL</label>
         <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Home</li>
-            <li>Home</li>
+          <li>
+            <a href="#" className="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
         </ul>
-    </nav>
-    
-  </>
-)
-
-}
+      </nav>
+    </>
+  );
+};
 
 export default Header;
