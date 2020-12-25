2ecdf6a89758af653ab526113799e041d8377f53
Alikhan219 <merabhai786786@gmail.com>
2020-12-25 09:12:51 -0800

    working on landing page

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -4,7 +4,13 @@ import React from "react"
 const Banner=()=>{
     return(
         <>
-            
+            <section>
+        <div class="banner">
+            <h2 class="main-txt">Amazing Adventure of Life</h2>
+            <h3 class="small-txt">wonderful&nbsp; books</h3>
+            <a href="#" class="banner-btn">Contact us</a>
+        </div>
+    </section> 
         </>
     )
 }
