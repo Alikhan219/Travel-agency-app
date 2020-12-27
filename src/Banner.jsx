f19a0ec0c0e6f41c043f7af61c5e91f2a9be0443
Alikhan219 <merabhai786786@gmail.com>
2020-12-27 07:27:40 -0800

    giving some styles to the header

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,16 +1,18 @@
-import React from "react"
+import React from "react";
 
-
-const Banner=()=>{
-    return(
-        <>
-            <section>
+const Banner = () => {
+  return (
+    <>
+      <section>
         <div class="banner">
-            <h2 class="main-txt">Amazing Adventure of Life</h2>
-            <h3 class="small-txt">wonderful&nbsp; books</h3>
-            <a href="#" class="banner-btn">Contact us</a>
+          <h2 class="main-txt">Amazing Adventure of Life</h2>
+          <h3 class="small-txt">wonderful&nbsp; Places</h3>
+          <a href="#" class="banner-btn">
+            Contact us
+          </a>
         </div>
-    </section> 
-        </>
-    )
-}
+      </section>
+    </>
+  );
+};
+export default Banner;
